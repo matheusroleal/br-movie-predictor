0eca7a9a6d3740999260abf8b25e04f1cda6e6cb
matheus.leal <matheus.leal@corp.globo.com>
2019-06-02 18:18:17 -0300

    PCR Update

--- mining/regression/pcr.R
+++ mining/regression/pcr.R
@@ -6,6 +6,8 @@ library(pls)
 csv = read.csv(file="dataset/RegressionAnalysisPreProcessing.csv", header=FALSE, sep=",")
 dt = data.frame(csv)
 
+y <- as.matrix(dt[,4])
+
 # fit model
 fit <- pcr(V4~., data=dt, validation="CV")
 
@@ -13,8 +15,12 @@ fit <- pcr(V4~., data=dt, validation="CV")
 summary(fit)
 
 # make predictions
-predictions <- predict(fit, dt, ncomp=4)
+y_predicted <- predict(fit, dt)
 
-# summarize accuracy
-mse <- mean(dt$V4 == predictions)
-print(mse)
+# Sum of Squares Total and Error
+sst <- sum((y - mean(y))^2)
+sse <- sum((y_predicted[,,1] - y)^2)
+
+# R squared
+rsq <- 1 - sse / sst
+rsq
