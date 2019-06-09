006a2fa2138d71448f804dacd5bf9bbf354724a5
matheus.leal <matheus.leal@corp.globo.com>
2019-06-09 11:24:02 -0300

    kNN minning update

--- mining/regression/knn.R
+++ mining/regression/knn.R
@@ -1,5 +1,5 @@
-# install.packages("ggplot2")
-library(ggplot2)
+# install.packages("caret")
+library(caret)
 
 # load data
 csv = read.csv(file="dataset/RegressionAnalysisPreProcessing.csv", header=FALSE, sep=",")
@@ -13,19 +13,19 @@ dt_test <- dt[-ran,]
 
 # The 2nd column of training dataset because that is what we need to predict about testing dataset
 # Also convert ordered factor to normal factor
-dt_target <- as.factor(dt_train[ran,4])
+dt_target <- as.factor(dt_train[,4])
 
 # The actual values of 2nd couln of testing dataset to compaire it with values that will be predicted
 # Also convert ordered factor to normal factor
-test_target <- as.factor(dt_test[-ran,4])
+test_target <- as.factor(dt_test[,4])
 
 # Run knn function
 library(class)
-pr <- knn(dt_train,dt_test,cl=dt_target,k=20)
+pr <- knn3Train(dt_train[,-4], dt_test[,-4], dt_target, k = 5, prob = TRUE)
 
 # Create the confucion matrix
-tb <- table(pr,test_target)
+tb <- as.matrix(table(test_target,pr))
 
-##check the accuracy
+# check the accuracy
 accuracy <- function(x){sum(diag(x)/(sum(rowSums(x)))) * 100}
 accuracy(tb)
